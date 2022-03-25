304d0f717a4c2570291befe8a8dd018190afa700
Matt-8A <moimportant.important@gmail.com>
2022-03-25 03:01:07 -0700

    Uploaded files to repository

---
+++ Instagram/LoginViewController.swift
@@ -0,0 +1,56 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Matthew Ochoa on 3/24/22.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let username = usernameField.text!
+        let password = passwordField.text!
+        
+        PFUser.logInWithUsername(inBackground: username, password: password) { user, error in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: Those credentials do not match...")
+            }
+        }
+    }
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+        user.signUpInBackground { success, error in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            } else {
+                print("Error: Couldn't create new User...")
+            }
+        }
+    }
+    
+    
+
+}
